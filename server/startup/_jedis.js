02a2dc3322a1ff83df8757a61f7136ce5443b015
berkich <berkich@cox.net>
2015-08-31 15:58:43 -0700

    Removed local update

--- server/startup/_jedis.js
+++ server/startup/_jedis.js
@@ -25,7 +25,7 @@ Meteor.startup( function() {
 		console.log('Error loading default settings: ' + err.message);
 	}
 
-	directoryService = new MockProvider();// DirectoryService(Jedis.settings.get('ldap'));
+	directoryService = DirectoryService(Jedis.settings.get('ldap'));
 	Jedis.accessManager = new AccessManager(directoryService);
 	Jedis.accessManager.loadAccessPermissions();
 
