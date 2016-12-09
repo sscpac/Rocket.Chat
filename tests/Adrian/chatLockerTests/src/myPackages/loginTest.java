0e56d87257782beb949180d1c3d986e9836ef73e
Adrian <adrianalonzo619@gmail.com>
2016-12-09 07:41:09 -0800

    Completed Registration & Refactored code
    Completed Unit Test for Registration and refactored the codes to make
    them less static and susceptible to chat locker changes.

--- tests/Adrian/chatLockerTests/src/myPackages/loginTest.java
+++ tests/Adrian/chatLockerTests/src/myPackages/loginTest.java
@@ -1,10 +1,14 @@
 package myPackages;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.Test;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.BeforeClass;
 import org.testng.AssertJUnit;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,12 +19,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 //These Unit Tests check for the Login feature it assumes the user of the
 //following credentials is valid
 //  username: adrian
-//  password: adrian
+//  password: AdriansSecretPassword
 //  email: adrian@gmail.com
      
 public class loginTest {
     public static WebDriver driver;
-     
+    
+    ///////||||||||||||| BEGIN EDITABLE TEXT ||||||||||||||||||||||||||||||||||||
+    public static String URL_CHATLOCKER_MAIN = "http://localhost:3000";
+    public static String VALID_USERNAME = "adrian";
+    public static String VALID_PASSWORD = "AdriansSecretPassword";
+    public static String VALID_EMAIL = "adrian@gmail.com";
+    public static String INVALID_USERNAME = "andnsan";
+    public static String INVALID_PASSWORD = "jsgdjhasd";
+    public static String INVALID_EMAIL = "jksfh@gmail.com";
+    public static String ERROR_MESSAGE = "User not found or incorrect password";
+    public static String HOME_TITLE = "Home";
+    ///////||||||||||||| END EDITABLE TEXT |||||||||||||||||||||||||||||||||||||
+    
     //declare all your elements here using By it will make your code cleaner and simple
     private By usernameOrEmailFieldLocator = By.id("emailOrUsername");
     private By passwordFieldLocator = By.id("pass");
@@ -36,16 +52,16 @@ public class loginTest {
      
      
     @BeforeClass //this means the method below will be called Once before this whole Class
-    public void setupTheDriverObject(){
+    public static void setupTheDriverObject(){
         driver = new SafariDriver();
     }
      
-    @BeforeMethod
+    @Before
 	public void getThePageWeWantToTest(){
         //I just refresh page since it begins each test with a controlled starting point
         //more in-depthunit testing within the app will have to manually login and navigate
         //to your components
-        driver.get("http://localhost:3000");
+        driver.get(URL_CHATLOCKER_MAIN);
         //this tells the driver to wait for the username/email field element to load otherwise it will fail the test within 10 secs
         new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(usernameOrEmailFieldLocator));
          
@@ -59,63 +75,63 @@ public class loginTest {
     @Test //1 //this is a Unit Test Identifier
     public void invalidUsernameWithValidPassLoginShouldFail() { //notice the Should Fail and descriptive name, dont be shy!
         //I simply pass in my predefined conditions that we planned in the table above
-        inputLoginFieldsWith("andnsan", "adrian"); //i call the method to simplify each test
+        inputLoginFieldsWith(INVALID_USERNAME, VALID_PASSWORD); //i call the method to simplify each test
         //this will wait for the notification banner to appear
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
         //when it does appear we will compare what we expect for it to display when a login fails
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //2
     public void ValidUsernameWithInvalidPasswordShouldFail() {
-        inputLoginFieldsWith("adrian", "jsgdjhasd");
+        inputLoginFieldsWith(VALID_USERNAME, INVALID_PASSWORD);
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //3
     public void invalidUsernameAndInvalidPasswordShouldFail() {
-        inputLoginFieldsWith("andnsan", "jsgdjhasd");
+        inputLoginFieldsWith(INVALID_USERNAME, INVALID_PASSWORD);
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //4
     public void invalidEmailWithValidPasswordShouldFail() {
-        inputLoginFieldsWith("jksfh@gmail.com", "adrian");
+        inputLoginFieldsWith(INVALID_EMAIL, VALID_PASSWORD);
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //5
     public void validEmailWithInvalidPasswordShouldFail() {
-        inputLoginFieldsWith("adrian@gmail.com", "hghjbjhg");
+        inputLoginFieldsWith(VALID_EMAIL, INVALID_PASSWORD);
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //6
     public void invalidEmailWithInvalidPasswordShouldFail() {
-        inputLoginFieldsWith("jksfh@gmail.com", "jgfhgvn");
+        inputLoginFieldsWith(INVALID_EMAIL, INVALID_PASSWORD);
         WebElement banner = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(BannerLocator));
-        AssertJUnit.assertEquals(banner.getText(), "User not found or incorrect password");
+        AssertJUnit.assertEquals(banner.getText(), ERROR_MESSAGE);
     }
      
     @Test //7
     public void ValidUsernameWithValidPasswordShouldPass() {
         //this case uses valid inputs to login
-        inputLoginFieldsWith("adrian", "adrian");
+        inputLoginFieldsWith(VALID_USERNAME, VALID_PASSWORD);
         //when we do login I will wait at least 10 seconds for the tile component in the next page to load which is <h2>Home</h2>
         WebElement HomeHeader = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(HomeHeaderLocator));
         //When it has loaded lets compare and ensure it is Home
-        AssertJUnit.assertEquals(HomeHeader.getText(), "Home");
+        AssertJUnit.assertEquals(HomeHeader.getText(), HOME_TITLE);
     }
      
     @Test //8
     public void ValidEmailWithValidPasswordShouldPass() {
-        inputLoginFieldsWith("adrian@gmail.com", "adrian");
+        inputLoginFieldsWith(VALID_EMAIL, VALID_PASSWORD);
         WebElement HomeHeader = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(HomeHeaderLocator));
-        AssertJUnit.assertEquals(HomeHeader.getText(), "Home");
+        AssertJUnit.assertEquals(HomeHeader.getText(), HOME_TITLE);
     }  
      
 }
