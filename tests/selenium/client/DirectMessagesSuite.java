182ed19090292669a74977b23c70c547667750c8
soniatran <soniatran1996@gmail.com>
2016-12-28 12:26:48 -0800

    Update DirectMessagesSuite.java
    Test 1 - findFriends works only sometimes, not sure why.

--- tests/selenium/client/DirectMessagesSuite.java
+++ tests/selenium/client/DirectMessagesSuite.java
@@ -1,4 +1,4 @@
-package myPackages;
+package testRocketChatPackage;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -19,7 +19,7 @@ import org.junit.Assert;
 
 
 
-public class DirectMsgStuff {
+public class DirectMessagesSuite {
 	
 	
 	public static WebDriver driver;
@@ -40,25 +40,30 @@ public class DirectMsgStuff {
 	private static By findFriendTextField = By.id("who");
 	private static By buttonCancel = By.cssSelector("button.button.cancel-direct-message");
 	private static By buttonCreate = By.cssSelector("button.button.primary.save-direct-message");
-	private static By buttonMoreDMsg = By.cssSelector("button.more.more-direct-messages");
-	private static By SearchDM = By.id("channel-search");
-	private static By sortOption = By.cssSelector("select#sort.c-select");
+	//private static By buttonMoreDMsg = By.cssSelector("button.more.more-direct-messages");
+	private static By buttonMoreDMsg = By.xpath("//*[@id=\"rocket-chat\"]/aside/div[2]/div/ul[2]/button");
+	
+	//private static By SearchDM = By.id("channel-search");
+	private static By SearchDM = By.cssSelector("input#channel-search.search");
+	
+	//private static By sortOption = By.cssSelector("select#sort.c-select");
 	private static By foundfriendDM = By.cssSelector("a.channel-link");
 	private static By textFriend = By.cssSelector("textarea.input-message.autogrow-short");
-	private static By sendMsgButt = By.cssSelector("i.icon-paper-plane");
+	private static By sendMessageButton = By.cssSelector("i.icon-paper-plane");
 	private static By panelLeft = By.cssSelector("div.rooms-list");
 	private static By autoCompleteOption = By.cssSelector("div.-autocomplete-container");
 	
 	private static By roomTitleLocator = By.className("room-title");
-	private static By closeDMButtonLocator = By.className("arrow close");
-	private static By DMselector = By.cssSelector("a.open-room");
+	//private static By closeDMButtonLocator = By.className("arrow.close");	//DOESN'T WORK - Just use buttonCancel
+	private static By createRoomOptions = By.cssSelector("a.open-room");
 	
 /*TEST Order:
  * 
  * Creating and Sending Direct Messages
- * 1. Valid Friend
- * 2. Invalid Friend
- * 3. More Direct Messages Option
+ * 1. Find Valid Friend
+ * 2. Invalid Friend (No Friend by that Name)
+ * 3. Search through More Direct Messages
+ * 4. Send Direct Message to Friend
  */
 	
 	private static boolean detectElement(By element) {
@@ -71,19 +76,79 @@ public class DirectMsgStuff {
 			return false;
 		}
 	}
+	
+	private static void clicksAchannelToMakeDirectMessagesNotActive() {
+		
+		WebElement firstChannel = driver.findElements(createRoomOptions).get(0);
+		firstChannel.click();
+		
+	}
+	
+	private void sendDMTo(String user, String message) throws Exception{
+		
+		driver.findElement(buttonMoreDMsg).click();
+		
+		Thread.sleep(2000);
+		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(SearchDM));
+		//driver.findElement(sortOption).click();	//An OPTION, but not yet bothered with
+		driver.findElement(SearchDM).clear();
+		driver.findElement(SearchDM).sendKeys(user);
+		
+		Thread.sleep(1000);
+		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(foundfriendDM));
+		driver.findElement(foundfriendDM).click();	
+		driver.findElement(textFriend).sendKeys(message);
+		driver.findElement(sendMessageButton).click();
+		
+	}
+	
+	private void searchForFriend(String searchForText) throws Exception {
+		//Click on Add DM
+		WebElement dmElement = driver.findElements(addRoomElement).get(1);
+		dmElement.click();
+		Thread.sleep(1500);
+		
+		//search for the friend
+		driver.findElement(findFriendTextField).clear();
+		driver.findElement(findFriendTextField).sendKeys(searchForText);
+		Thread.sleep(1000);
+		
+		if(detectElement(autoCompleteOption)){
+			driver.findElement(autoCompleteOption).click();
+		}
+		else{
+			driver.findElement(By.id("Value")).sendKeys(Keys.RETURN);
+		}
+		driver.findElement(buttonCreate).click();
+		
+	}
+
+	private void searchInDM(String user) throws Exception{
+		
+		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(panelLeft));
+		driver.findElement(buttonMoreDMsg).click();
+		
+		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(SearchDM)); //Doesn't work so Thread.sleep is used for now
+		Thread.sleep(2000);
+		driver.findElement(SearchDM).clear();
+		driver.findElement(SearchDM).sendKeys(user);	
+		
+		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(foundfriendDM));
+		Thread.sleep(1000);
+		driver.findElement(foundfriendDM).click();
+	}
 
 	@BeforeClass
 	public static void setupTest() throws Exception{
 		
-//		System.setProperty(
-//				"webdriver.chrome.driver",
-//				"/home/osboxes/Documents/Selenium Library/chromedriver.exe"
-//				);
-//
-//
-//		driver = new ChromeDriver();
+		System.setProperty(
+				"webdriver.chrome.driver",
+				"/home/osboxes/Documents/Selenium Library/chromedriver.exe"
+				);
+
+		driver = new ChromeDriver();
 		
-		driver = new SafariDriver();
+//		driver = new SafariDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
 		driver.get(Home);
@@ -93,184 +158,74 @@ public class DirectMsgStuff {
 		driver.findElement(password).sendKeys(loginPW);
 		driver.findElement(loginButton).click();
 		//Thread.sleep(500);
-		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(addRoomElement));
+		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(addRoomElement));
 		
 		
 	}
 	
 	@Before
-	public void checkDMMenu() {
+	public void checkDMMenu() throws Exception {
 		
-		//driver.get(Home);
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(addRoomElement));
+//		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(panelLeft));
+		clicksAchannelToMakeDirectMessagesNotActive();
+		Thread.sleep(1000);
 
 	}
 	
-	@After
-	public void resetFromPreviousTest(){
-		//do this incase of a fail to close the menu from a previous test
-		if(detectElement(closeDMButtonLocator)){
-			driver.findElement(closeDMButtonLocator).click();
-		}
-	}
-	
-	private void searchInDM(String user) throws Exception{
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(panelLeft));
-		driver.findElement(buttonMoreDMsg).click();
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(SearchDM));
-		//driver.findElement(sortOption).click();
-		driver.findElement(SearchDM).clear();
-		driver.findElement(SearchDM).sendKeys(user);	
-		
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(foundfriendDM));
-		Thread.sleep(1000);
-		driver.findElement(foundfriendDM).click();
-		
-//		int size = driver.findElements(DMselector).size();
-//		
-//		for(int i = 0; i <= size; i++){
-//			if(driver.findElements(DMselector).get(i).getAttribute("title") == user){
-//				driver.findElements(DMselector).get(i).click();
-//				break;
-//			}
-//		}
-	}
-	
-	private void sendDMTo(String user, String message) throws Exception{
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(panelLeft));
-		driver.findElement(buttonMoreDMsg).click();
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(SearchDM));
-		//driver.findElement(sortOption).click();
-		driver.findElement(SearchDM).clear();
-		driver.findElement(SearchDM).sendKeys(user);	
-		//Thread.sleep(2000);
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(foundfriendDM));
-		driver.findElement(foundfriendDM).click();	
-		driver.findElement(textFriend).sendKeys(message);
-		driver.findElement(sendMsgButt).click();
-		
-	}
-	
-	private void searchForFriend(String searchForText) throws Exception {
-		//Click on Add DM
-		WebElement dmElement = driver.findElements(addRoomElement).get(1);
-		dmElement.click();
-		
-		//search for the friend
-		driver.findElement(findFriendTextField).clear();
-		driver.findElement(findFriendTextField).sendKeys(searchForText);
-		//Thread.sleep(3000);
-		//click otherwise enter
-		//driver.findElement(By.id("Value")).sendKeys(Keys.ENTER);
-		driver.findElement(autoCompleteOption).click();
-//		if(detectElement(autoCompleteOption)){
-//			driver.findElement(autoCompleteOption).click();
-//		}else{
-//			driver.findElement(By.id("Value")).sendKeys(Keys.RETURN);
-//		}
-		driver.findElement(buttonCreate).click();
-		
-	}
-	
-	
 	@Test // 1
 	public void findFriend() throws Exception {
-		
-		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(addRoomElement));
-		//WebElement dmElement = driver.findElements(addRoomElement).get(1);
-		//dmElement.click();
-		//driver.findElement(findFriendTextField).sendKeys(ValidFriend);
-		
-//		if (detectElement(autoCompleteOption)) {
-//			
-//			driver.findElement(autoCompleteOption).click();
-//			
-//		}
-//		else {
-//			
-//			driver.findElement(By.id("Value")).sendKeys(Keys.RETURN);
-//			
-//		}
-//		
-//		driver.findElement(buttonCreate).click();
+
 		searchForFriend(ValidFriend);
 		Thread.sleep(2000);
-		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(roomTitleLocator));
+		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(roomTitleLocator)); //DOES NOT WORK - Use Thread.sleep for now
 		String actualRoomTitle = driver.findElement(roomTitleLocator).getAttribute("innerText");
 		System.out.println(actualRoomTitle);
 		Assert.assertEquals(actualRoomTitle, ValidFriend);
-		//driver.findElement(textFriend).sendKeys("Hi " + ValidFriend + ", can I have straight A's for Christmas?");
-		//driver.findElement(sendMsgButt).click();
-		
-		//System.out.println("You have successfully messaged " + ValidFriend + ".");
 		
 	}
 	
 	@Test // 2
 	public void noFriendByThatName() throws Exception {
 		
-		//String InvalidFriend = UUID.randomUUID().toString().substring(0, 8);
-//		driver.findElement(findFriendTextField).sendKeys(InvalidFriend);
-//		driver.findElement(buttonCreate).click();
-//		Thread.sleep(2000);
-		
-//		driver.findElement(buttonCancel).click();
-		
-		//Detects if "Cancelling" goes back to the page where they can can choose from Channels/DMs
-//		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(addRoomElement));
-		
-//		System.out.println(InvalidFriend + " could not be found.");
 		searchForFriend(InvalidFriend);
 		String actualSearchResult = driver.findElement(autoCompleteOption).getText();
 		System.out.println(actualSearchResult);
 		Assert.assertThat(actualSearchResult, CoreMatchers.containsString("Nothing found."));
-		driver.findElement(closeDMButtonLocator).click();
+		driver.findElement(buttonCancel).click();
+		
 	}
 	
 	@Test // 3
 	public void searchThroughDM() throws Exception{
 		
 		searchInDM(ValidFriend);
-		
-		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(panelLeft));
-		//driver.findElement(buttonMoreDMsg).click();
-		
-		//Thread.sleep(1000);
-		
-		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(sortOption));
-		//driver.findElement(sortOption).click();
-		
-		//driver.findElement(SearchDM).sendKeys(ValidFriend);		
-		
-		//Thread.sleep(1000);
-		
-		//new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(foundfriendDM));
-		//driver.findElement(foundfriendDM).click();
-		
-		//driver.findElement(textFriend).sendKeys("Found you " + ValidFriend + "!");
-		//driver.findElement(sendMsgButt).click();
-		
-		//Thread.sleep(3000);
-		
 		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(roomTitleLocator));
 		String actualRoomTitle = driver.findElement(roomTitleLocator).getAttribute("innerText");
 		System.out.println(actualRoomTitle);
 		Assert.assertEquals(actualRoomTitle, ValidFriend);
+		
 	}
 	
 	@Test //4	//need to somehow add Assert 
 	public void sendDMToFriend() throws Exception{
+		
 		sendDMTo(ValidFriend, "I found you!");
-//		new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(roomTitleLocator));
-//		String actualRoomTitle = driver.findElement(roomTitleLocator).getAttribute("innerText");
-//		System.out.println(actualRoomTitle);
-//		Assert.assertEquals(actualRoomTitle, ValidFriend);
+		//Assert.assertEquals(actualRoomTitle,  ValidFriend);
+		
 	}
 	
+//	@After
+//	public void resetFromPreviousTest(){
+//		//do this incase of a fail to close the menu from a previous test
+//		if(detectElement(buttonCancel)){
+//			driver.findElement(buttonCancel).click();
+//		}
+//		System.out.println("Bye");
+//	}
 	
 	@AfterClass
 	public static void endTesting(){
-		
+
 		driver.close();
 		driver.quit();
 	}
