7c356f991bfa10b668fccc7408ee69a1c640c700
Philip Jeffries <philipjeffriespdx@pointloma.edu>
2016-12-29 15:32:07 -0800

    Initial Suite Structure

--- tests/selenium/client/rightmenu/RoomInfo.java
+++ tests/selenium/client/rightmenu/RoomInfo.java
@@ -7,7 +7,7 @@
  * only works if the channel is the first and only channel created
  */
 
-package testRocketChatPackage.rightmenu;
+package myPackage;
 
 import org.junit.*;
 import org.openqa.selenium.By;
@@ -34,6 +34,9 @@ public class RoomInfo {
     public static String VALID_EMAIL = "validEmail@gmail.com";
     public static String CHANNEL_NAME = "channelname";
     public static String CHANNEL_NAME2 = "second_name";
+    public static String TOPIC_1 = "topic1";
+    public static String TOPIC_2 = "topic2";
+
     private By usernameOrEmailFieldLocator = By.id("emailOrUsername");
     private By passwordFieldLocator = By.id("pass");
     private By loginButtonLocator = By.className("button primary login");
@@ -59,7 +62,13 @@ public class RoomInfo {
     private By nameFieldLocator = By.xpath("//*[@name=\"name\"]");
     
     //FOR CHANGING TOPIC
-    
+    //id('rocket-chat')/x:div[3]/x:div/x:div/x:section[2]/x:div/x:div/x:form/x:ul/x:li[2]/x:div/x:span[2]/x:i
+    private By topicPencilLocator = By.xpath("//*[@data-edit=\"topic\"]");
+    //private By topicPencilLocator = By.xpath("//*[@id=\"rocket-chat\"]/div[3]/div/div/section[2]/div/div/form/ul/li[2]/div/span[2]/i");
+    private By topicLocator = By.xpath("//*[@id=\"rocket-chat\"]/div[3]/div/div/section[2]/div/div/form/ul/li[2]/div/span[1]");
+    private By topicCancelLocator =  By.xpath("//*[@class=\"button cancel\"]");
+    private By topicSaveLocator =  By.xpath("//*[@class=\"button primary save\"]");
+    private By topicFieldLocator = By.xpath("//*[@name=\"topic\"]");
 
     private void inputLoginFieldsWith(String usernameOrEmail, String password){
         driver.findElement(usernameOrEmailFieldLocator).sendKeys(usernameOrEmail);
@@ -93,18 +102,19 @@ public class RoomInfo {
         driver.quit(); 
     }
 
-
-    @Test //1  Changing Channel Name
+//Test 1 
+    @Test //1  Tests full functionality of Changing the Name of the Channel
     public void test1() { 
         driver.findElements(infoIconLocator).get(1).click();
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameLocator));
         
         //Get current Value
         String originalName = driver.findElement(nameLocator).getText();
+        //SETS THE STARTING VALUE
         if(originalName != "channelname")
         {
         	try {
-                Thread.sleep(1000);                 
+                Thread.sleep(1000);  //without using sleep Selenium will not wait for the pencils to load, even with the WebDriverWait               
             } catch(InterruptedException ex) {
                 Thread.currentThread().interrupt();
             }
@@ -137,41 +147,104 @@ public class RoomInfo {
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameLocator));
     	Assert.assertEquals(originalName, driver.findElement(nameLocator).getText());
     	
+    	
     	//Select edit
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(namePencilLocator));
         driver.findElement(namePencilLocator).click();
-    	
         //Change value
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameFieldLocator));
         driver.findElement(nameFieldLocator).sendKeys(CHANNEL_NAME2);
-        
         //Cancel
     	driver.findElement(nameCancelLocator).click();
-
     	//Check value
     	new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameLocator));
     	Assert.assertEquals(originalName, driver.findElement(nameLocator).getText());
     	
+    	
     	//Select edit
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(namePencilLocator));
         driver.findElement(namePencilLocator).click();
-    	
         //Change value
         new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameFieldLocator));
         driver.findElement(nameFieldLocator).sendKeys(CHANNEL_NAME2);
-        
         //Save
     	driver.findElement(nameSaveLocator).click();
-    	
     	//Check value
     	new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameLocator));
     	Assert.assertEquals(CHANNEL_NAME2, driver.findElement(nameLocator).getText());
+    	
+    	
+    	//Set Channel Name back to original
+    	//Select edit
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(namePencilLocator));
+        driver.findElement(namePencilLocator).click();
+        //Change value
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(nameFieldLocator));
+        driver.findElement(nameFieldLocator).sendKeys(originalName);
+        //Save
+    	driver.findElement(nameSaveLocator).click();
     }
-    /* 
-    @TODO
-
-    @Test //2 Topic
     
+
+    @Test //2 Tests full functionality of Changing the Topic of the Channel
+    public void test2() { 
+        driver.findElements(infoIconLocator).get(1).click();
+        
+        //Select edit
+        try { 
+            Thread.sleep(2000);             
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    	new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicPencilLocator));
+        driver.findElement(topicPencilLocator).click();
+        //Change value
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicFieldLocator));
+        driver.findElement(topicFieldLocator).sendKeys(TOPIC_1);
+        //Save
+    	driver.findElement(topicSaveLocator).click();
+    	
+    	
+    	//Select edit
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicPencilLocator));
+        driver.findElement(topicPencilLocator).click();
+        //Cancel
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicCancelLocator));
+    	driver.findElement(topicCancelLocator).click();
+    	//Check value
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicLocator));
+    	Assert.assertEquals(TOPIC_1, driver.findElement(topicLocator).getText());
+    	
+    	
+    	//Select edit
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicPencilLocator));
+        driver.findElement(topicPencilLocator).click();
+        //Change value
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicFieldLocator));
+        driver.findElement(topicFieldLocator).sendKeys(TOPIC_2);
+        //Cancel
+    	driver.findElement(topicCancelLocator).click();
+    	//Check value
+    	new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicLocator));
+    	Assert.assertEquals(TOPIC_1, driver.findElement(topicLocator).getText());
+    	
+    	
+    	//Select edit
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicPencilLocator));
+        driver.findElement(topicPencilLocator).click();
+        //Change value
+        new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicFieldLocator));
+        driver.findElement(topicFieldLocator).sendKeys(TOPIC_2);
+        //Save
+    	driver.findElement(topicSaveLocator).click();
+    	//Check value
+    	new WebDriverWait(driver, 5).until(ExpectedConditions.presenceOfElementLocated(topicLocator));
+    	Assert.assertEquals(TOPIC_2, driver.findElement(topicLocator).getText());
+    }
+    
+    //@TODO
+
+    /*
     @Test //3 Description
     
     @Test //4 Type
