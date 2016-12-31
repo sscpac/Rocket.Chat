72abcb1aaac0dfe390833c592967a46fd47bcc77
Adrian Alonzo <adrianalonzo619@gmail.com>
2016-12-30 23:32:12 -0800

    Refactored flex nav to use xpaths and no thread.sleeps.

--- tests/selenium/client/rightmenu/rightMenuNavTest.java
+++ tests/selenium/client/rightmenu/rightMenuNavTest.java
@@ -1,5 +1,7 @@
 package testRocketChatPackage.rightmenu;
  
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,105 +23,97 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class rightMenuNavTest {
     public static WebDriver driver;     
  
-    private static By usernameOrEmailFieldLocator = By.id("emailOrUsername");
-    private static By passwordFieldLocator = By.id("pass");
-    private static By loginButtonLocator = By.className("button primary login");
-    private static By arrowButtonLocator = By.className("arrow bottom");
     private By channelsLocator = By.className("name");
     private By flexTabBarLocator = By.className("flex-tab-bar");
-    private static By flexTabButtonsLocator = By.className("tab-button");
     private static By currentTabTitleLocator = By.xpath("//*[@id=\"rocket-chat\"]/div[3]/div/div/section/div/div/div/h2");
     private static String tabTitle;
     
-    private static void inputLoginFieldsWith(String usernameOrEmail, String password){
-        driver.findElement(usernameOrEmailFieldLocator).sendKeys(usernameOrEmail);
-        driver.findElement(passwordFieldLocator).sendKeys(password);
-        driver.findElement(loginButtonLocator).click();
-    }
-     
-    //BeforeClass
+    private static By roomInfoTab = By.xpath("//div[contains(@title, 'Room Info')]");
+    private static By searchTab = By.xpath("//div[contains(@title, 'Search')]");
+    private static By memberListTab = By.xpath("//div[contains(@title, 'Members List')]");
+    private static By notificationTab = By.xpath("//div[contains(@title, 'Notifications')]");
+    private static By filesTab = By.xpath("//div[contains(@title, 'Files List')]");
+    private static By mentionsTab = By.xpath("//div[contains(@title, 'Mentions')]");
+    private static By starredMsgTab = By.xpath("//div[contains(@title, 'Starred Messages')]");
+    private static By pinnedMsgTab = By.xpath("//div[contains(@title, 'Pinned Messages')]");
+    
+
     @BeforeClass //this means the method below will be called Once before this whole Class
     public static void setupTheDriverObject(){
         //driver setup with
     	driver = new SafariDriver();
-        driver.get("http://localhost:3000");
-        //this tells the driver to wait for the username/email field element to load otherwise it will fail the test within 10 secs
-        (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(usernameOrEmailFieldLocator));
-        inputLoginFieldsWith("test", "test");
+    	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    	testRocketChatPackage.login.loginTest.login("test", "test", driver);
     }
     
-    //Before
     @Before 
     public void getThePageWeWantToTest() {
-   
         //Page Load
-        (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(arrowButtonLocator));
-        //Click on 1st channel
         driver.findElements(channelsLocator).get(0).click();
-        (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(flexTabBarLocator));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(flexTabBarLocator));
     }
     
-    //AfterClass
-    @AfterClass //this will be called After ALL tests complete
+    @AfterClass 
     public static void closeDriver(){
  
-    	driver.quit(); //closes driver and browser window
+    	driver.quit(); 
     }
     
-    private static void clickOnMenuItem(int index) throws InterruptedException{
-    	Thread.sleep(500);
-    	driver.findElements(flexTabButtonsLocator).get(index).click(); 
-    	Thread.sleep(500);
-    	WebElement TabElement = (new WebDriverWait(driver,3)).until(ExpectedConditions.presenceOfElementLocated(currentTabTitleLocator));
-    	tabTitle = TabElement.getText();
+    private void openTab(By tab){
+    	driver.findElement(tab).click(); 
+    	tabTitle = driver.findElement(currentTabTitleLocator).getText();
+    	System.out.println(tabTitle);
     }
     
     //Tests
-    @Test //1st tab
-    public void Tab1AsRoomInfoShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(1);
+    @Test 
+    public void openRoomInfoTab() { 
+    	openTab(roomInfoTab);
     	Assert.assertEquals(tabTitle, "Room Info");	
     }
     
-    @Test //2nd tab
-    public void Tab2AsSearchMessagesShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(2);
+    //@TODO for some odd issue tabTitle = Starred Messages
+    @Test 
+    public void openSearchMessagesTab() { 
+    	openTab(searchTab);
     	Assert.assertEquals(tabTitle, "Search Messages");  	
     }
     
-    @Test //3rd tab
-    public void Tab3AsMembersListShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(3);
+    @Test 
+    public void openMemberListTab() { 
+    	openTab(memberListTab);
     	Assert.assertEquals(tabTitle, "Members List"); 	
     }
     
-    @Test //4th tab
-    public void Tab4AsNotificationsShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(4);
+    @Test
+    public void openNotificationsTab() { 
+    	openTab(notificationTab);
     	Assert.assertEquals(tabTitle, "Notifications");  	
     }
     
-    @Test //5th tab
-    public void Tab5AsFilesListShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(4);
+    @Test 
+    public void shouldOpenFilesTab() { 
+    	openTab(filesTab);
     	Assert.assertEquals(tabTitle, "Files List");   	
     }
     
-    @Test //6th tab
-    public void Tab6AsMentionsShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(4);
+    //@TODO for some odd issue tabTitle = Room Info
+    @Test 
+    public void openMentions() { 
+    	openTab(mentionsTab);
     	Assert.assertEquals(tabTitle, "Mentions"); 	
     }
     
-    @Test //7th tab
-    public void Tab7AsStarredMessagesShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(4);
+    @Test 
+    public void openStarredMessages() { 
+    	openTab(starredMsgTab);
     	Assert.assertEquals(tabTitle, "Starred Messages");  	
     }
     
-    @Test //8th tab
-    public void Tab8AsPinnedMessagesShouldPass() throws InterruptedException { 
-    	clickOnMenuItem(4);
+    //@TODO for some odd issue tabTitle = Mentions
+    @Test 
+    public void openPinnedMessage()  { 
+    	openTab(pinnedMsgTab);
     	Assert.assertEquals(tabTitle, "Pinned Messages"); 	
     }
     
